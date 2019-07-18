7f148f81ea232b6b1959d5235660e034da3a589f
yinshiknag <41133718+yinshiknag@users.noreply.github.com>
2019-07-18 15:25:28 +0800

    1.0
    示例

---
+++ data/sky.js
@@ -0,0 +1,7 @@
+//"tiles":["https://webst03.is.autonavi.com/appmaptile?style=6&x={x}&y={y}&z={z}"],//高德影像
+
+//"tiles":["http://mt0.google.cn/vt/lyrs=s&x={x}&y={y}&z={z}"]//谷歌地图
+
+//"url":"mapbox://mapbox.satellite",//mapbox官方
+
+http://222.128.14.106:3857/gisproxy/tile?table=xcjrj0326&x={x}&y={y}&z={z}//紫光
---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+	$(".ulnav .navli:eq(0)").click(function(){
+		$("#navid").css({"display" : "none"});
+		
+	})
+	$(".ulnav .navli:eq(1)").click(function(){
+		$("#navid").css({"display" : "none"});
+		
+	})
+	$(".ulnav .navli:eq(2)").click(function(){
+		$("#navid").css({"display" : "none"});
+		
+	})
+	$(".ulnav .navli:eq(3)").click(function(){
+		$("#navid").css({"display" : "none"});
+		
+	})
+	$(".ulnav .navli:eq(4)").click(function(){
+		$("navid").css({"display" : "none"});
+		
+	})
+})
